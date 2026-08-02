0586174dc9d60e8001d1226e1c6eb30bcd3587ec
Dániel Veress <daniel.veress3@gmail.com>
2026-08-02 17:38:54 +0200

    feat(dbt): update staging models to handle point-in-time correctness

--- dbt/models/staging/stg_entsoe__actual_load.sql
+++ dbt/models/staging/stg_entsoe__actual_load.sql
@@ -4,18 +4,22 @@ with source as (
 renamed as (
     select
         cast(ts as timestamptz) as ts_utc,
+        ingested_at,
+        revision_num,
         load as load_mw
     from source
 ),
 deduped as (
-    select 
-        ts_utc, 
+    select
+        ts_utc,
+        ingested_at,
         load_mw,
         row_number() over (
-            partition by ts_utc order by load_mw
+            partition by ts_utc
+            order by ingested_at desc, revision_num desc
         ) as row_num
     from renamed
 )
-select ts_utc, load_mw 
+select ts_utc, ingested_at, load_mw 
 from deduped
 where row_num = 1
